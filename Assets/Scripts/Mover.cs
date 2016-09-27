93dfb3b9aeac60e52e42d1047aeb35cd76857389
Coyote <mike.w.coyote@gmail.com>
2016-09-26 22:00:07 -0300

    Game Setup, player and camera

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour 
+{
+	private Rigidbody rb;
+	public float speed;
+
+	void Awake ()
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
+	// Use this for initialization
+	void Start () 
+	{
+		rb.velocity = transform.forward * speed;
+	}
+
+}
